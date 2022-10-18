f94e51bdd4b3763f25b2d535885cb9531dce9d37
git <e1808378@cmaisonneuve.qc.ca>
2022-10-18 18:25:52 -0400

    Header home icon (non fonctionnel) -https://github.com/hcharob/chicken-devs-web/issues/10

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <?php wp_head() ?>
     <?php show_admin_bar(true); ?>
@@ -18,7 +19,7 @@
 
     <header class="site__header" id="site__header">
         <section class="site__logo">
-            <?php the_custom_logo(); ?>
+            <i class="fa fa-home" aria-hidden="true"></i>
         </section>
         <section class="site__nav">
             <?php get_template_part('composents/template','mainNav');?>
