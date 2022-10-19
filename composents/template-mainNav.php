46df38c792fa8ce6ef3c3e9bf853bf3e0dcd9dbc
git <e1808378@cmaisonneuve.qc.ca>
2022-10-19 14:13:17 -0400

    Header full developped (hover effect manquant) Fixes #10

--- composents/template-mainNav.php
+++ composents/template-mainNav.php
@@ -6,6 +6,12 @@
      * @subpackage cidw-4w4
      */
 ?>
+             <input type="checkbox"  id="chk-burger">
+                <label for="chk-burger" id="burger" class="burger">
+                    <div class="burger__ligne"></div>
+                    <div class="burger__ligne"></div>
+                    <div class="burger__ligne"></div>
+                </label>
             <?php
                 wp_nav_menu(array("menu"=>"principal", "container" => "nav"));
             ?>
