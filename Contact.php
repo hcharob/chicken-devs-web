22549c499909721a425182da264388e9a534a6cb
Kevin Hoang <e1950899@cmaisonneuve.qc.ca>
2022-11-15 12:30:14 -0500

    Fonctionnalité de base: hover sélection hexagonale

--- Contact.php
+++ Contact.php
@@ -1,9 +1,15 @@
+<!-- Truc à faire: faire en sorte que la quantité de div correspond au nombre de profs dans Wordpress -->
+
 <?php /* Template Name: contact */ ?>
 <?php get_header() ?>
-    <main class="site_main">
+    <main class="site__main">
         <section class="profs">
-            <div>
-                
+            <div class="profs_canvas">
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
             </div>
         </section>
     </main>
