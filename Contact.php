6d0972ec6c72a14d45f77d368416ccfd7ab1ccbd
Kevin Hoang <e1950899@cmaisonneuve.qc.ca>
2022-11-18 17:17:47 -0500

    Ajout des images (non-opt), pas de pixel, test BG

--- Contact.php
+++ Contact.php
@@ -1,10 +1,20 @@
 <!-- Truc à faire: faire en sorte que la quantité de div correspond au nombre de profs dans Wordpress -->
+<!-- Truc à faire: faire en sorte que chaque élément de div associe un «prof» dans Wordpress -->
 
 <?php /* Template Name: contact */ ?>
 <?php get_header() ?>
     <main class="site__main">
         <section class="profs">
             <div class="profs_canvas">
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
+                <div class="profs_selection"></div>
                 <div class="profs_selection"></div>
                 <div class="profs_selection"></div>
                 <div class="profs_selection"></div>
