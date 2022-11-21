c51665b18c67b2145b9055a01db174b7ce09c5bf
hoangkev231 <hoangkev231@gmail.com>
2022-11-21 00:45:01 -0500

    Display grid, ajout des nouveaux div et class

--- Contact.php
+++ Contact.php
@@ -6,20 +6,30 @@
     <main class="site__main">
         <section class="profs">
             <div class="profs_canvas">
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
-                <div class="profs_selection"></div>
+                <div class="profs_info">
+                    <h1 class="profs_info_nom">Ahmed Boudjani</h1> <!-- Test du nom -->
+                    <p class="profs_info_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aperiam perferendis voluptate saepe architecto odit maiores similique autem. Facilis necessitatibus assumenda autem eaque veritatis sunt quia dicta. Odit, numquam accusantium?</p>
+                </div>
+
+                <div class="profs_selection">
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                    <div class="profs_choix"></div>
+                </div>
+                
+                <div class="profs_fond"></div>
+
             </div>
         </section>
     </main>
