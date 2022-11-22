1e2d01a6d2776a5b23ad5afdfb14447cd50a9bb8
Kevin Hoang <e1950899@cmaisonneuve.qc.ca>
2022-11-22 10:19:08 -0500

    Début des media queries

--- contact.php
+++ contact.php
@@ -1,6 +1,3 @@
-<!-- Truc à faire: faire en sorte que la quantité de div correspond au nombre de profs dans Wordpress -->
-<!-- Truc à faire: faire en sorte que chaque élément de div associe un «prof» dans Wordpress -->
-
 <?php /* Template Name: contact */ ?>
 <?php get_header() ?>
     <main class="site__main">
@@ -10,6 +7,10 @@
                     <h1 class="profs_info_nom">Ahmed Boudjani</h1> <!-- Test du nom -->
                     <p class="profs_info_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aperiam perferendis voluptate saepe architecto odit maiores similique autem. Facilis necessitatibus assumenda autem eaque veritatis sunt quia dicta. Odit, numquam accusantium?</p>
                 </div>
+
+                <div class="profs_fond"></div>
+
+                <!-- <div class="profs_texte-choix">Sélectionnez un prof</div> -->
 
                 <div class="profs_selection">
                     <div class="profs_choix"></div>
@@ -27,8 +28,6 @@
                     <div class="profs_choix"></div>
                     <div class="profs_choix"></div>
                 </div>
-                
-                <div class="profs_fond"></div>
 
             </div>
         </section>
