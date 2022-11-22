650bb56bd058458aabf9a0107ffb99b529c1846a
git <e1808378@cmaisonneuve.qc.ca>
2022-11-22 10:45:44 -0500

    Commit Final incomplet des element d'acceuil

--- composantes/template-previewSectionProjects.php
+++ composantes/template-previewSectionProjects.php
@@ -1,4 +1,4 @@
-        <section class="pages__sections__section section1">
+        <section class="pages__sections__section section2">
             <div class="hoverPannel__conteneur">   
                 <div class="hoverPannel__contour">
                     <div class="hoverPannel__contain">
@@ -14,6 +14,7 @@
                                             stroke: #a663ff;
                                             stroke-miterlimit: 10;
                                             stroke-width: 2px;
+                                            color: $couleur_rosePrimaire;
                                         }
                                     </style>
                                 </defs>
