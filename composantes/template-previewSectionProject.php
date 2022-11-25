3c02fc871adba0c3db94244e0fda24b63c1dac80
Kevin Hoang <hoangkev231@gmail.com>
2022-11-25 16:54:29 -0500

    Début S4 avec les extras médias de WP

---
+++ composantes/template-previewSectionProject.php
@@ -0,0 +1,34 @@
+        <section class="pages__sections__section section2">
+            <div class="hoverPannel__conteneur">   
+                <div class="hoverPannel__contour">
+                    <div class="hoverPannel__contain">
+                        <div class="box__titre">
+                            <h2 class="titre">
+                            <?php echo get_field('section_h1')?><br><span><?php echo get_field('section_h1_span')?></span>
+                            </h2>
+                            <svg class="projets__barre" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 353.41 26.71">
+                                <defs>
+                                    <style>
+                                        .cls-1 {
+                                            fill: none;
+                                            stroke: #a663ff;
+                                            stroke-miterlimit: 10;
+                                            stroke-width: 2px;
+                                            color: $couleur_rosePrimaire;
+                                        }
+                                    </style>
+                                </defs>
+                                <line class="cls-1" y1="13.36" x2="328" y2="13.36" />
+                                <rect class="cls-1" x="331.62" y="4.91" width="16.89" height="16.89"
+                                    transform="translate(109.04 -236.55) rotate(45)" />
+                            </svg>
+                        </div>
+                        <a class="button_wrap" href="<?php echo get_permalink(); ?>">
+                            <div class="Long_Button">
+                                <?php echo get_field('section_button_text') ?>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
