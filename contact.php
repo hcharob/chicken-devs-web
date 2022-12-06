c25811de410dcad4885791aba8146136cb757b97
Kevin Hoang <hoangkev231@gmail.com>
2022-12-06 12:03:48 -0500

    Balise img (fonctionne pas), display flex aux info

--- contact.php
+++ contact.php
@@ -3,14 +3,15 @@
     <main class="site__main">
         <section class="profs">
             <div class="profs_canvas">
-                <div class="profs_info">
-                    <h1 class="profs_info_nom">Ahmed Boudjani</h1> <!-- Test du nom -->
-                    <p class="profs_info_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aperiam perferendis voluptate saepe architecto odit maiores similique autem. Facilis necessitatibus assumenda autem eaque veritatis sunt quia dicta. Odit, numquam accusantium?</p>
+
+                <div class="profs_case">
+                    <!-- <div class="profs_fond"></div> -->
+                    <img src="images/profs/portrait/ahm3.JPG" alt="Ahmed">
+                    <div class="profs_info">
+                        <h1 class="profs_info_nom">Ahmed Boudjani</h1>
+                        <p class="profs_info_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aperiam perferendis voluptate saepe architecto odit maiores similique autem. Facilis necessitatibus assumenda autem eaque veritatis sunt quia dicta. Odit, numquam accusantium?</p>
+                    </div>
                 </div>
-
-                <div class="profs_fond"></div>
-
-                <!-- <div class="profs_texte-choix">Sélectionnez un prof</div> -->
 
                 <div class="profs_selection">
                     <div class="profs_choix"></div>
