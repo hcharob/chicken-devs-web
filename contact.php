6e1831d9b3ee4d9eb4b974d8b7149fc585f34ac9
Kevin Hoang <hoangkev231@gmail.com>
2022-12-09 16:53:31 -0500

    L'image de Jeremy Clarkson fonctionne apparement

--- contact.php
+++ contact.php
@@ -5,8 +5,7 @@
             <div class="profs_canvas">
 
                 <div class="profs_case">
-                    <!-- <div class="profs_fond"></div> -->
-                    <img src="images/profs/portrait/ahm3.JPG" alt="Ahmed">
+                    <img class="profs_image" src="https://m.media-amazon.com/images/M/MV5BMTk4Mjk3NTAzOF5BMl5BanBnXkFtZTgwMDAwOTA4MDI@._V1_SX600_.jpg" alt="Ahmed">
                     <div class="profs_info">
                         <h1 class="profs_info_nom">Ahmed Boudjani</h1>
                         <p class="profs_info_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aperiam perferendis voluptate saepe architecto odit maiores similique autem. Facilis necessitatibus assumenda autem eaque veritatis sunt quia dicta. Odit, numquam accusantium?</p>
