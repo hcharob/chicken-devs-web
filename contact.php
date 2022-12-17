7c23b2a913db23c99e7aa7c1287bb4bf5c9b5fa2
git <e1808378@cmaisonneuve.qc.ca>
2022-12-17 00:27:04 -0500

    merge de la branch sprint4 et KevSprint4(branch a kev incomplet)

--- contact.php
+++ contact.php
@@ -1,14 +1,35 @@
-<!-- Truc à faire: faire en sorte que la quantité de div correspond au nombre de profs dans Wordpress -->
-<!-- Truc à faire: faire en sorte que chaque élément de div associe un «prof» dans Wordpress -->
-
 <?php /* Template Name: contact */ ?>
 <?php get_header() ?>
     <main class="site__main">
+        <div class="projets__boite__titre">
+            <h2 class="projets__titre">
+                L'équipe <span>TIM</span>
+            </h2>
+            <svg class="projets__barre" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 353.41 26.71">
+                <defs>
+                    <style>
+                        .cls-1 {
+                            fill: none;
+                            stroke: #a663ff;
+                            stroke-miterlimit: 10;
+                            stroke-width: 2px;
+                        }
+                    </style>
+                </defs>
+                <line class="cls-1" y1="13.36" x2="70" y2="13.36" />
+                <rect class="cls-1" x="331.62" y="4.91" width="16.89" height="16.89"
+                    transform="translate(-150 -236.55) rotate(45)" />
+            </svg>
+        </div>
         <section class="profs">
             <div class="profs_canvas">
-                <div class="profs_info">
-                    <h1 class="profs_info_nom">Ahmed Boudjani</h1> <!-- Test du nom -->
-                    <p class="profs_info_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aperiam perferendis voluptate saepe architecto odit maiores similique autem. Facilis necessitatibus assumenda autem eaque veritatis sunt quia dicta. Odit, numquam accusantium?</p>
+
+                <div class="profs_case">
+                    <img class="profs_image" src="https://m.media-amazon.com/images/M/MV5BMTk4Mjk3NTAzOF5BMl5BanBnXkFtZTgwMDAwOTA4MDI@._V1_SX600_.jpg" alt="Ahmed">
+                    <div class="profs_info">
+                        <h1 class="profs_info_nom">Ahmed Boudjani</h1>
+                        <!-- <p class="profs_info_desc">La description</p> -->
+                    </div>
                 </div>
 
                 <div class="profs_selection">
@@ -26,10 +47,9 @@
                     <div class="profs_choix"></div>
                     <div class="profs_choix"></div>
                     <div class="profs_choix"></div>
+                    <div class="profs_choix">Tous les membres</div>
                 </div>
                 
-                <div class="profs_fond"></div>
-
             </div>
         </section>
     </main>
